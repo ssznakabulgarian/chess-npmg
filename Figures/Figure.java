7135504e95b0ff78ec619611a4ca79ea3539ffe0
sv4t <zlatkov2004@abv.bg>
2023-02-13 09:25:20 +0200

    added info to constructor

--- Figures/Figure.java
+++ Figures/Figure.java
@@ -1,11 +1,13 @@
 package Figures;
 
 import Common.Position;
+import GameLogic.Board;
 
 public abstract class Figure {
     private Position StartingPosition;
     private Position position;
     private boolean Colour;
+    private Board board;
 
     public Position getStartingPosition() {
         return StartingPosition;
@@ -15,8 +17,10 @@ public abstract class Figure {
         return Colour;
     }
 
-    public Figure(Position startingPosition, boolean Color){
-
+    public Figure(Position startingPosition, boolean Color, Board board){
+        startingPosition = this.StartingPosition;
+        Color = this.Colour;
+        board = this.board;
     }
 
     abstract boolean isMoveValid(Position newPosition);
