b9414c907d2fde3d49ae593271ef61ba25cf70e3
VanyaMB <vbalkan04@abv.bg>
2023-02-14 09:34:17 +0200

    Bishop almost done, needs testing

--- Figures/Bishop.java
+++ Figures/Bishop.java
@@ -1,4 +1,49 @@
 package Figures;
 
-public class Bishop {
+import Common.Colour;
+import Common.Position;
+import GameLogic.Board;
+
+public class Bishop extends Figure{
+
+
+    public Bishop(Position startingPosition, Colour Color, Board board) {
+        super(startingPosition, Color, board);
+    }
+    //array 8x8, empty spaces are zeroes
+    Figure arr[][] = board.getField();
+    @Override
+    boolean isMoveValid(Position newPosition) {
+        if(!board.isSquareEmpty(newPosition)){
+            /*if(board.getFigureAt(newPosition).equals(Figures.King)){
+
+            }*/
+            if (arr[newPosition.x][newPosition.y].getColour().equals(arr[getPosition().x][getPosition().y])){
+                return false;
+            }
+        }
+        int x = getPosition().x - newPosition.x;
+        if(x<0) x=x*(-1);
+        int y = getPosition().y - newPosition.y;
+        if(y<0) y=y*(-1);
+        if(x == y){
+
+            for (int i = 0; i<x; i++){
+                Position between = null;
+                between.setX(getPosition().x+i);
+                between.setY(getPosition().y+i);
+                if (!board.isSquareEmpty(between)){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+
+
 }
