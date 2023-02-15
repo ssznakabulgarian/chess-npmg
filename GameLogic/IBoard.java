8491c4d31edfa10ebfc15dd008b4cb1d8d4f958d
Georgi Baldjiev <gbaldjiev2004@gmail.com>
2023-02-15 08:55:59 +0200

    minor changes

--- GameLogic/IBoard.java
+++ GameLogic/IBoard.java
@@ -4,9 +4,13 @@ import Common.Colour;
 import Common.Position;
 import Figures.Figure;
 
+import java.util.List;
+
 public interface IBoard {
     boolean isSquareEmpty(Position position);
     Figure getFigureAt(Position position);
     Colour getPlayerToMove();
     Figure[][] getField();
+    List<Figure> getFigures();
+    Figure getSelectedFigure();
 }
