126bfdd3ff6002081aaf49da5cf568cbe4aa0cf7
yordandd <danidokov@gmail.com>
2023-02-16 08:16:49 +0200

    NAPRAVIHME MOVE URAA

--- Figures/King.java
+++ Figures/King.java
@@ -6,21 +6,27 @@ import GameLogic.Board;
 
 public class King extends Figure {
     public boolean isKingMoved;
-
     public King(Position position, Colour colour, Board board) {
         super(position, colour, board);
     }
 
+    public boolean isInCheck(){
+
+        return false;
+    }
+
     @Override
     public boolean isMoveValid(Position newPosition){
-        if(board.isSquareEmpty(newPosition)) {
-            return true;
-        }
-        if(!board.isSquareEmpty(newPosition)) {
-            if(board.getFigureAt(newPosition).getColour() == colour) {
-                return false;
-            }else {
+        if(position.getX() - newPosition.getX() <= 1  && position.getX() - newPosition.getX()  >= 0 &&  position.getY() - newPosition.getY() <= 1  && position.getY() - newPosition.getY()  >= 0 ) {
+            if (board.isSquareEmpty(newPosition)) {
                 return true;
+            }
+            if (!board.isSquareEmpty(newPosition)) {
+                if (board.getFigureAt(newPosition).getColour() == colour) {
+                    return false;
+                } else {
+                    return true;
+                }
             }
         }
         return false;
