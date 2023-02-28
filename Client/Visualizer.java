a2c131f61701cce437002d6d8ec7f2ab722c6130
Ina Galova <ina.galova@gmail.com>
2023-02-28 11:27:30 +0200

    Board Visualizer figures added

--- Client/Visualizer.java
+++ Client/Visualizer.java
@@ -1,21 +1,34 @@
 package Client;
 import Common.Position;
+import Figures.Figure;
+import GameLogic.Board;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Visualizer extends JFrame {
     JFrame frame = new JFrame("Chess");
     JPanel panel = new JPanel();
+
     private final int WindowUpperMargin = 30;
+    private final int WindowLeftMargin = 5;
+    private final int fieldSize = 100;
     private Position selectedAt;
+    private static Board board=new Board();
+    private final BufferedImage whiteBishop, whiteRook, whiteKnight, whiteKing, whiteQueen, whitePawn, blackBishop, blackRook, blackKnight, blackKing, blackQueen, blackPawn;
 
-    public Visualizer() {
+    public Visualizer(Board board) throws IOException {
         super("Chess");
+        Visualizer.board = board;
 
-        setSize(800, 800+WindowUpperMargin);
+        setSize(800+WindowLeftMargin, 800+WindowUpperMargin);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
@@ -41,24 +54,50 @@ public class Visualizer extends JFrame {
 
             }
         });
+
+        URL blackBishopImageURL = getClass().getResource("/sources/figures/bishopB.png");
+        blackBishop = ImageIO.read(Objects.requireNonNull(blackBishopImageURL));
+        URL blackRookImageURL = getClass().getResource("/sources/figures/rookB.png");
+        blackRook = ImageIO.read(Objects.requireNonNull(blackRookImageURL));
+        URL blackKnightImageURL = getClass().getResource("/sources/figures/knightB.png");
+        blackKnight = ImageIO.read(Objects.requireNonNull(blackKnightImageURL));
+        URL blackKingImageURL = getClass().getResource("/sources/figures/kingB.png");
+        blackKing = ImageIO.read(Objects.requireNonNull(blackKingImageURL));
+        URL blackQueenImageURL = getClass().getResource("/sources/figures/queenB.png");
+        blackQueen = ImageIO.read(Objects.requireNonNull(blackQueenImageURL));
+        URL blackPawnImageURL = getClass().getResource("/sources/figures/pawnB.png");
+        blackPawn = ImageIO.read(Objects.requireNonNull(blackPawnImageURL));
+
+        URL whiteBishopImageURL = getClass().getResource("/sources/figures/bishopW.png");
+        whiteBishop = ImageIO.read(Objects.requireNonNull(whiteBishopImageURL));
+        URL whiteRookImageURL = getClass().getResource("/sources/figures/rookW.png");
+        whiteRook = ImageIO.read(Objects.requireNonNull(whiteRookImageURL));
+        URL whiteKnightImageURL = getClass().getResource("/sources/figures/knightW.png");
+        whiteKnight = ImageIO.read(Objects.requireNonNull(whiteKnightImageURL));
+        URL whiteKingImageURL = getClass().getResource("/sources/figures/kingW.png");
+        whiteKing = ImageIO.read(Objects.requireNonNull(whiteKingImageURL));
+        URL whiteQueenImageURL = getClass().getResource("/sources/figures/queenW.png");
+        whiteQueen = ImageIO.read(Objects.requireNonNull(whiteQueenImageURL));
+        URL whitePawnImageURL = getClass().getResource("/sources/figures/pawnW.png");
+        whitePawn = ImageIO.read(Objects.requireNonNull(whitePawnImageURL));
     }
 
     private void onFieldClicked(MouseEvent e){
-        Position clickPosition = new Position(e.getX()/100, (e.getY()-WindowUpperMargin)/100);
+        Position clickPosition = new Position((e.getX()-WindowLeftMargin)/fieldSize, (e.getY()-WindowUpperMargin)/fieldSize);
         if (selectedAt == null) {
             selectedAt = clickPosition;
-            repaint(selectedAt.getX() * 100, (selectedAt.getY() * 100) + WindowUpperMargin, 100, 100);
+            repaint(selectedAt.getX() * fieldSize + WindowLeftMargin, (selectedAt.getY() * fieldSize) + WindowUpperMargin, fieldSize, fieldSize);
             return;
         }
         if (selectedAt.equals(clickPosition)){
             selectedAt = null;
-            repaint(clickPosition.getX() * 100, (clickPosition.getY() * 100) + WindowUpperMargin, 100, 100);
+            repaint(clickPosition.getX() * fieldSize + WindowLeftMargin, (clickPosition.getY() * fieldSize) + WindowUpperMargin, fieldSize, fieldSize);
             return;
         }
         Position oldSelection = new Position(selectedAt);
         selectedAt=clickPosition;
-        repaint(selectedAt.getX()*100, (selectedAt.getY()*100)+WindowUpperMargin, 100, 100);
-        repaint(oldSelection.getX()*100, (oldSelection.getY()*100)+WindowUpperMargin, 100, 100);
+        repaint(selectedAt.getX()*fieldSize+WindowLeftMargin, (selectedAt.getY()*fieldSize)+WindowUpperMargin, fieldSize, fieldSize);
+        repaint(oldSelection.getX()*fieldSize + WindowLeftMargin, (oldSelection.getY()*fieldSize)+WindowUpperMargin, fieldSize, fieldSize);
     }
 
     void drawBoard(Graphics g) {
@@ -67,11 +106,24 @@ public class Visualizer extends JFrame {
             for (int j = 0; j < 8; j++) {
                 if ((i+j)%2==0) graphics.setColor(Color.WHITE);
                 else {
-                    graphics.setColor(Color.BLACK);
+                    graphics.setColor(Color.darkGray);
                 }
-                graphics.fillRect(i*100, j*100+WindowUpperMargin, 100, 100);
+                graphics.fillRect(i*fieldSize + WindowLeftMargin, j*fieldSize+WindowUpperMargin, fieldSize, fieldSize);
             }
         }
+    }
+
+   void drawFigures(Graphics2D g) {
+       for (Figure figure : board.getFigures()){
+           BufferedImage img = null;
+           try {
+               img = (BufferedImage) getClass().getDeclaredField(figure.getColour().toString().toLowerCase()+figure.getClass().getSimpleName()).get(this);
+               g.drawImage(img, figure.getPosition().x * fieldSize + WindowLeftMargin,figure.getPosition().y * fieldSize+WindowUpperMargin,fieldSize,fieldSize,null);
+           } catch (IllegalAccessException | NoSuchFieldException e) {
+               e.printStackTrace();
+           }
+       }
+
     }
 
 
@@ -81,15 +133,20 @@ public class Visualizer extends JFrame {
         drawBoard(g);
         if (selectedAt != null) {
             graphics.setColor(Color.RED);
-            graphics.fillRect(selectedAt.getX()*100, (selectedAt.getY()*100)+WindowUpperMargin, 100, 100);
+            graphics.fillRect(selectedAt.getX()*100 + WindowLeftMargin, (selectedAt.getY()*100)+WindowUpperMargin, 100, 100);
         }
+        drawFigures(graphics);
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new Visualizer().setVisible(true);
+                try {
+                    new Visualizer(board).setVisible(true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
