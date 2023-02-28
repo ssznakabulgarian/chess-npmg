6848026cc69c83eda5b479aea4a159f3271c9929
Georgi Baldjiev <gbaldjiev2004@gmail.com>
2023-02-28 08:57:19 +0200

    added jakarta and gson dependency; impl. socket server; some minor edits

--- Figures/Figure.java
+++ Figures/Figure.java
@@ -4,28 +4,23 @@ import Common.Colour;
 import Common.InvalidMoveException;
 import Common.Position;
 import GameLogic.Board;
-import GameLogic.IBoard;
 
 public abstract class Figure {
     protected boolean hasMoved = false;
     protected Position position;
     protected Colour colour;
     protected Board board;
-
     public Position getPosition() {
         return position;
     }
-
     public Colour getColour() {
         return colour;
     }
-
     public Figure(Position position, Colour colour, Board board){
         this.position = position;
         this.colour = colour;
         this.board = board;
     }
-
     abstract boolean isMoveValid(Position newPosition);
     public void Move(Position newPosition) throws InvalidMoveException {
         Position oldPosition = new Position(position);
