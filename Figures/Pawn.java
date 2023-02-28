a74ea856136417ed25083081f7161f3048b10b94
sv4t <zlatkov2004@abv.bg>
2023-02-28 09:36:31 +0200

    an pesant? left

--- Figures/Pawn.java
+++ Figures/Pawn.java
@@ -4,7 +4,7 @@ import Common.Colour;
 import Common.Position;
 import GameLogic.Board;
 
-public class Pawn extends Figure{
+public class Pawn extends Figure {
     public Pawn(Position position, Colour Color, Board board) {
         super(position, Color, board);
     }
@@ -19,11 +19,31 @@ public class Pawn extends Figure{
             return false;
 
         // check if on newPosition is a same colour figure
-        if(board.getField()[newPosition.getX()][newPosition.getY()] != null && board.getField()[newPosition.getX()][newPosition.getY()].getColour().equals(getColour()))
+        if (board.getField()[newPosition.getX()][newPosition.getY()] != null
+                && board.getField()[newPosition.getX()][newPosition.getY()].getColour().equals(getColour()))
             return false;
 
+        // check if 1 space forward is a valid move
+        if (board.getField()[getPosition().getX()][newPosition
+                .getY()] == board.getField()[getPosition().getX()][getPosition().getY() + 1]
+                && board.getField()[getPosition().getX()][getPosition().getY() + 1] == null)
+            return true;
 
-        if(getHasMoced())
+        // check if 2 spaces forward is a valid move
+        if (!getHasMoved() && board.getField()[getPosition().getX()][newPosition
+                .getY()] == board.getField()[getPosition().getX()][getPosition().getY() + 2]
+                && board.getField()[getPosition().getX()][getPosition().getY() + 1] == null
+                && board.getField()[getPosition().getX()][getPosition().getY() + 2] == null)
+            return true;
+
+        // check if pawn can take
+        if (board.getField()[newPosition.getX()][newPosition.getY()] != null
+                && (board.getField()[newPosition.getX()][newPosition
+                        .getY()] == board.getField()[getPosition().getX() + 1][getPosition().getY() + 1]
+                        ||
+                        board.getField()[newPosition.getX()][newPosition
+                                .getY()] == board.getField()[getPosition().getX() - 1][getPosition().getY() + 1]))
+            return true;
 
         return false;
     }
