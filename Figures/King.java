a34091401d02749dd4079f9e3f5548d1f5bd476e
yordandd <danidokov@gmail.com>
2023-03-02 09:44:40 +0200

    aoha

--- Figures/King.java
+++ Figures/King.java
@@ -1,11 +1,12 @@
 package Figures;
 
 import Common.Colour;
+import Common.InvalidMoveException;
 import Common.Position;
 import GameLogic.Board;
 
 public class King extends Figure {
-    public boolean isKingMoved;
+
     public King(Position position, Colour colour, Board board) {
         super(position, colour, board);
     }
@@ -26,8 +27,42 @@ public class King extends Figure {
         }
         return false;
     }
+    @Override
+    public void Move(Position position) throws InvalidMoveException, InvalidMoveException {
 
+        Figure otherFigure = board.getFigureAt(position);
+        if(otherFigure instanceof Rook
+                && otherFigure.getColour().equals(this.colour)) castle((Rook, position) otherFigure); //castle
+        else super.Move(position);
+    }
 
+    public void castle(Rook rook, Position newPosition){
+        if(rook.getPosition().getX() > position.getX()){
+            int x1 = position.getX();
+            int x2 = rook.getPosition().getX();
+        }else {
+            int x1 = rook.getPosition().getX();
+            int x2 = position.getX();
+        }
+        if(!King.hasMoved && !rook.hasMoved){
+            boolean inCheck = true;
+            for (Figure Figure : figures) {
+                for(int x = x1; x <= x2; x++) {
+                    if(Figure.getColour() != this.colour){
+                        Position check = new Position(x, position.getY());
+                        if(Figure.isMoveValid(check)){
+                            inCheck = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            if(inCheck){
+                position = newPosition;
+            }
+
+        }
+    }
 
 
 }
