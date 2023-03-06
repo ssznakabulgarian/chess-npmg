4abd1d01ad1b1efc0338c35d2ea6573afc64f4da
sv4t <zlatkov2004@abv.bg>
2023-03-06 20:45:08 +0200

    rook restructured

--- src/Figures/Rook.java
+++ src/Figures/Rook.java
@@ -8,70 +8,31 @@ public class Rook extends Figure {
     public Rook(Position position, Colour Color, Board board) {
         super(position, Color, board);
     }
-
     @Override
     public boolean isMoveValid(Position newPosition) {
-
-        // check if newPosition has valid coordinates
-        if (newPosition.getX() < 0 || newPosition.getY() < 0 || newPosition.getX() > 7 || newPosition.getY() > 7)
-            return false;
-        if (getPosition().equals(newPosition))
-            return false;
-        if (getPosition().getX() != newPosition.getX() && getPosition().getY() != newPosition.getY())
+        // edge cases
+        if (newPosition.getX() < 0 || newPosition.getY() < 0 || newPosition.getX() > 7 || newPosition.getY() > 7
+                || getPosition().equals(newPosition) || (getPosition().getX() != newPosition.getX() && getPosition().getY() != newPosition.getY())
+                || (board.getField()[newPosition.getX()][newPosition.getY()] != null
+                && board.getField()[newPosition.getX()][newPosition.getY()].getColour().equals(getColour())))
             return false;
 
-        // check if on newPosition is a same colour figure
-        if(board.getField()[newPosition.getX()][newPosition.getY()] != null && board.getField()[newPosition.getX()][newPosition.getY()].getColour().equals(getColour()))
-            return false;
-
-        // determine whether a move is vertical or horizontal
-        int sameCoordX = -1, sameCoordY = -1;
-        if (getPosition().getX() == newPosition.getX())
-            sameCoordX = getPosition().getX();
-        if (getPosition().getY() == newPosition.getY())
-            sameCoordY = getPosition().getY();
-
+        int loopHelper = -1;
+        // determine whether a move is vertical or horizontal (true: vertical; false: horizontal)
+        boolean direction = getPosition().getX() == newPosition.getX();
 
         // check if a figure is in the way
-        if (sameCoordX != -1) {
-            if (getPosition().getY() < newPosition.getY()) {
-                for (int i = getPosition().getY() + 1; i <= newPosition.getY(); i++) {
-                    if (board.getField()[sameCoordX][i] != null && i != newPosition.getY()) {
-                        return false;
-                    }
-                }
-                return true;
-
-            } else {
-                for (int i = getPosition().getY() - 1; i >= newPosition.getY(); i--) {
-                    if (board.getField()[sameCoordX][i] != null && i != newPosition.getY()) {
-                        return false;
-                    }
-                }
-                return true;
+        if (direction) {
+            if (getPosition().getY() < newPosition.getY()) loopHelper = 1;
+            for (int i = getPosition().getY() + loopHelper; i != newPosition.getY(); i+=loopHelper) {
+                if (board.getField()[getPosition().getX()][i] != null && i != newPosition.getY()) return false;
+            }
+        }else {
+            if (getPosition().getX() < newPosition.getX()) loopHelper = 1;
+            for (int i = getPosition().getX() + loopHelper; i != newPosition.getX(); i+=loopHelper) {
+                if (board.getField()[i][getPosition().getY()] != null && i != newPosition.getX()) return false;
             }
         }
-
-        if (sameCoordY != -1) {
-            if (getPosition().getX() < newPosition.getX()) {
-                for (int i = getPosition().getX() + 1; i <= newPosition.getX(); i++) {
-                    if (board.getField()[i][sameCoordY] != null && i != newPosition.getX()) {
-                        return false;
-                    }
-                }
-                return true;
-
-            } else {
-                for (int i = getPosition().getX() - 1; i >= newPosition.getX(); i--) {
-                    if (board.getField()[i][sameCoordY] != null && i != newPosition.getX()) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-        }
-
-        return false;
+        return true;
     }
-
 }
