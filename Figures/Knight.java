878043a06d9c775b1c24f9f0bc689915f5917669
Ina Galova <ina.galova@gmail.com>
2023-03-06 08:34:56 +0200

    knight fix

--- Figures/Knight.java
+++ Figures/Knight.java
@@ -15,35 +15,35 @@ public class Knight extends Figure{
             boardArray[newPosition.x][newPosition.y].getColour() != this.colour)
         {
             if (getPosition().x+2 < 8 && getPosition().y+1 < 8 &&
-                boardArray[newPosition.x][newPosition.y] == boardArray[getPosition().x+2][getPosition().y+1]) {
+                newPosition.x == getPosition().x+2 && newPosition.y == getPosition().y+1) {
                 return true;
             }
             if (getPosition().x+2 < 8 && getPosition().y-1 > -1 &&
-                boardArray[newPosition.x][newPosition.y] == boardArray[getPosition().x+2][getPosition().y-1]) {
+                newPosition.x == getPosition().x+2 && newPosition.y == getPosition().y-1) {
                 return true;
             }
             if (getPosition().x+1 < 8 && getPosition().y+2 < 8 &&
-                boardArray[newPosition.x][newPosition.y] == boardArray[getPosition().x+1][getPosition().y+2]) {
+                newPosition.x == getPosition().x+1 && newPosition.y == getPosition().y+2) {
                 return true;
             }
             if (getPosition().x+1 < 8 && getPosition().y-2 > -1 &&
-                boardArray[newPosition.x][newPosition.y] == boardArray[getPosition().x+1][getPosition().y-2]) {
+                newPosition.x == getPosition().x+1 && newPosition.y == getPosition().y-2) {
                 return true;
             }
             if (getPosition().x-1 > -1 && getPosition().y+2 < 8 &&
-                boardArray[newPosition.x][newPosition.y] == boardArray[getPosition().x-1][getPosition().y+2]) {
+                newPosition.x == getPosition().x-1 && newPosition.y == getPosition().y+2) {
                 return true;
             }
             if (getPosition().x-1 > -1 && getPosition().y-2 > -1 &&
-                boardArray[newPosition.x][newPosition.y] == boardArray[getPosition().x-1][getPosition().y-2]) {
+                newPosition.x == getPosition().x-1 && newPosition.y == getPosition().y-2) {
                 return true;
             }
             if (getPosition().x-2 > -1 && getPosition().y+1 < 8 &&
-                boardArray[newPosition.x][newPosition.y] == boardArray[getPosition().x-2][getPosition().y+1]) {
+                newPosition.x == getPosition().x-2 && newPosition.y == getPosition().y+1) {
                 return true;
             }
             if (getPosition().x-2 > -1 && getPosition().y-1 > -1 &&
-                boardArray[newPosition.x][newPosition.y] == boardArray[getPosition().x-2][getPosition().y-1]) {
+                newPosition.x == getPosition().x-2 && newPosition.y == getPosition().y-1) {
                 return true;
             }
         }
